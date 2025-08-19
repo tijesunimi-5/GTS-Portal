292a816f2586770bb837f2cb754db8fef518dc69
tijesunimi-5 <tijesunimiidowu16@gmail.com>
2025-08-19 18:23:50 +0100

    fixed the student[id] route

--- app/api/students/[id]/route.ts
+++ app/api/students/[id]/route.ts
@@ -1,16 +1,18 @@
-// app/api/students/[id]/route.ts
-
 import { NextRequest, NextResponse } from 'next/server';
 import User from '@/model/User';
-import {connectDB} from '@/lib/mongodb';
+import { connectDB } from '@/lib/mongodb';
 
-export async function PUT(
-  req: NextRequest,
-  { params }: { params: { id: string } }
-) {
+// Reusable Route Params type
+interface Params {
+  params: {
+    id: string;
+  };
+}
+
+export async function PUT(req: NextRequest, context: Params) {
   try {
     await connectDB();
-    const { id } = params;
+    const { id } = context.params;
     const updatedUserData = await req.json();
 
     const updatedUser = await User.findByIdAndUpdate(id, updatedUserData, {
@@ -21,13 +23,11 @@ export async function PUT(
       return NextResponse.json({ message: 'User not found' }, { status: 404 });
     }
 
-    console.log(`User with ID ${id} updated successfully.`);
     return NextResponse.json({
       message: 'User updated successfully',
       data: updatedUser,
     });
   } catch (error) {
-    console.error('Error updating user:', error);
     return NextResponse.json(
       { message: 'Error updating user', error },
       { status: 500 }
@@ -35,13 +35,10 @@ export async function PUT(
   }
 }
 
-export async function DELETE(
-  req: NextRequest,
-  { params }: { params: { id: string } }
-) {
+export async function DELETE(req: NextRequest, context: Params) {
   try {
     await connectDB();
-    const { id } = params;
+    const { id } = context.params;
 
     const deletedUser = await User.findByIdAndDelete(id);
 
@@ -49,14 +46,11 @@ export async function DELETE(
       return NextResponse.json({ message: 'User not found' }, { status: 404 });
     }
 
-    console.log(`User with ID ${id} deleted successfully.`);
     return NextResponse.json({ message: 'User deleted successfully' });
   } catch (error) {
-    console.error('Error deleting user:', error);
     return NextResponse.json(
       { message: 'Error deleting user', error },
       { status: 500 }
     );
   }
 }
-
